5f1a529fbffaca1c83d07e873793873d5b4ee485
CC0043 - PROGRAMAÇAO PARA DISPOSITIVOS MOVEIS <mobile@tailandia.k05s>
2023-04-17 08:31:39 -0300

    creating atividades branch and adding its files

--- src/components/common/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ActivityIndicator } from 'react-native';
-import { useTheme } from 'styled-components/native';
-
-export default function Loading() {
-  const theme = useTheme();
-
-  return <ActivityIndicator size={'large'} color={theme.COLORS.PRIMARY_500} />;
-}
